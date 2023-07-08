57d76c770397f4dbb7bfc7bc6700843227ac00a8
tawfik boujeh <tboujeh@gmail.com>
2023-07-08 16:56:09 +0200

    Initial Commit

---
+++ tap tap/tap_tapApp.swift
@@ -0,0 +1,17 @@
+//
+//  tap_tapApp.swift
+//  tap tap
+//
+//  Created by Tawfik Boujeh on 08.07.23.
+//
+
+import SwiftUI
+
+@main
+struct tap_tapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
